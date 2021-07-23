10bef9a29ab9a6a26f1100cdd0f18c3f15ea85e5
winnekes <simona.winnekes@googlemail.com>
2021-07-23 14:30:25 +0200

    Only listen with Google when prompted

--- listener.py
+++ listener.py
@@ -3,18 +3,30 @@ import time
 
 r = sr.Recognizer()
 
-# only listen via google if steve is called
-
-def listen(commands_queue):
+def listen(actively_listening, set_actively_listening, commands_queue):
     try:
         with sr.Microphone() as m:
             r.adjust_for_ambient_noise(m)
             print("Listening")
             audio = r.listen(m, phrase_time_limit=5)
             print("Parsing")
-            command = r.recognize_google(audio)
-            commands_queue.put(command)
-            print("I think you said: " + command)
+
+            if actively_listening is False:
+                command = r.recognize_sphinx(audio, keyword_entries=[("steve", 0)]).strip().lower()
+
+                if command == "steve":
+                    commands_queue.put("listening")
+                    set_actively_listening(True)
+
+            if actively_listening is True:
+                print("Listening with Google now")
+                command = r.recognize_google(audio).strip().lower()
+                commands_queue.put(command)
+                print("I think you said: " + command)
+
+                if command.lower().strip() == "stop listening":
+                    commands_queue.put("not listening")
+                    set_actively_listening(False)
 
     except sr.UnknownValueError:
         print("I could not understand you!")
@@ -23,6 +35,12 @@ def listen(commands_queue):
         print("I don't feel so good, t-t-try again later; {0}".format(e))
 
 def start_listener(commands_queue):
+    actively_listening = False
+
+    def set_actively_listening(value):
+        nonlocal actively_listening
+        actively_listening = value
+
     while True:
-        listen(commands_queue)
+        listen(actively_listening, set_actively_listening, commands_queue)
         time.sleep(.1)
